2a0285f12c5e36de79a027d25d6a656917ac6ba2
Madhura Mande <mandemadhura@gmail.com>
2020-05-17 12:24:27 +0530

    Refactor RabbitMQ Receiver

--- rabbitmq_receiver.py
+++ rabbitmq_receiver.py
@@ -4,34 +4,106 @@ Receiver Module
 
 import json
 import sys
+from abc import ABCMeta, abstractmethod
 
 import pika
 
-
 QUEUE_NAME = "hello"
 
-def callback(channel, method, properties, body):
-    """Callback method for basic consume of RabbitMQ"""
+class Receiver(metaclass=ABCMeta):
+    '''Abstract Base class for all receiver classes'''
 
-    received_data = json.loads(body)
-    print(f"{received_data['sender']}: {received_data['msg']}")
+    DEFAULT_HOST = 'localhost'
+    DEFAULT_PORT = '5672'
+
+    def __init__(self, _host, _port):
+        '''Init Method'''
+        self._host = _host or self.DEFAULT_HOST
+        self._port = _port or self.DEFAULT_PORT
+
+    @abstractmethod
+    def connect(self):
+        '''Establishes a new connection'''
+        raise NotImplementedError
+
+    @abstractmethod
+    def receive(self):
+        '''Receives a message'''
+        raise NotImplementedError
 
 
-if len(sys.argv) == 2:
-    QUEUE_NAME = sys.argv[1]
+class RabbitMQReceiver(Receiver):
+    '''Receives a message using RabbitMQ'''
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+    EXCHANGE_TYPE = 'fanout'
 
-channel.queue_declare(queue=QUEUE_NAME)
-channel.exchange_declare(exchange='tarpan', exchange_type='fanout', durable=True)
-channel.queue_bind(queue=QUEUE_NAME, exchange='tarpan', routing_key='tarpan')
-channel.basic_consume(on_message_callback=callback, queue=QUEUE_NAME)
+    def __init__(self, **kwargs):
+        '''Init Method'''
 
+        super().__init__(kwargs.get('host'), kwargs.get('port'))
+        self._exchange_name = kwargs.get('exchange_name')
+        self._queue = kwargs.get('queue')
+        self._routing_key = self._exchange_name
+        self._auto_ack = True
+        self._connection = None
+        self._channel = None
 
-channel.basic_consume(
-    queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
+    def connect(self):
+        '''Establishes a new connection to RabbitMQ'''
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+        try:
+            self._connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self._host, port=self._port))
+            self._channel = self._connection.channel()
+            self._channel.queue_declare(queue=self._queue)
+            self._channel.exchange_declare(exchange=self._exchange_name,
+                                           exchange_type=self.EXCHANGE_TYPE,
+                                           durable=True)
+            self._channel.queue_bind(queue=self._queue,
+                                     exchange=self._exchange_name,
+                                     routing_key=self._routing_key)
+        except Exception as err:
+            print(f'Error: {err}')
+
+    def receive(self):
+        '''Receives a message from RabbitMQ exchange and queue'''
+
+        try:
+            self._channel.basic_consume(
+                queue=self._queue,
+                on_message_callback=RabbitMQReceiver.callback,
+                auto_ack=self._auto_ack)
+
+            print(' [*] Waiting for messages. To exit press CTRL+C')
+            self._channel.start_consuming()
+        except Exception as err:
+            print(f'Error: {err}')
+
+    @staticmethod
+    def callback(channel, method, properties, body):
+        """Callback method for basic consume of RabbitMQ"""
+
+        received_data = json.loads(body)
+        print(f"{received_data['sender']}: {received_data['msg']}")
+
+def usage():
+    '''Prints the usage '''
+
+    print('usage: rabbitmq_receiver.py queue_name \n \
+          queue_name: Name of the queue')
+    sys.exit(1)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print('Plese enter a valid argument')
+        usage()
+
+    # TODO: Get the username from the command line and
+    # form a queue_name from it
+    queue_name = sys.argv[1]
+
+    receiver = RabbitMQReceiver(host='localhost', port=5672,
+                                exchange_name='tarpan', queue=queue_name)
+    receiver.connect()
+    receiver.receive()
